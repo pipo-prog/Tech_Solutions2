ccb6561bced9a29e37878324484dcf359d66756a
pipo-prog <85861741+pipo-prog@users.noreply.github.com>
2026-08-15 12:34:54 -0400

    Add files via upload

---
+++ database/migrations/2026_07_26_152830_create_proyectos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->date('fecha_inicio');
+            $table->string('estado');
+            $table->string('responsable');
+            $table->decimal('monto', 15, 2);
+            $table->foreignId('created_by')->constrained('usuarios')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proyectos');
+    }
+};
